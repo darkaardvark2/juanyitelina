fc637695973fc82e70e87274847142b9ea498471
Juan Farfan <darkaardvark2@gmail.com>
2014-01-13 16:06:33 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Juanyitelina::Application.config.secret_key_base = '0d4f96e5db3ae91170600e3aa88867276227b427b81352cf4a708249d6796edd4ed111f56283c3eb30611ea46f83bd57d02bfc2ee676e43d540a94018197d332'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Juanyitelina::Application.config.session_store :cookie_store, key: '_juanyitelina_session'
